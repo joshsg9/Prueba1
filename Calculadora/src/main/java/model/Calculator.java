92b8c5e2f1bf45f0f27605c4dfb8072fc2fbd868
josh.uni99@gmail.com <josh.uni99@gmail.com>
2024-08-14 21:57:38 -0500

    Multi 1

--- Calculadora/src/main/java/model/Calculator.java
+++ Calculadora/src/main/java/model/Calculator.java
@@ -4,5 +4,13 @@ public class Calculator {
 
     public Calculator() {
     }
+
+    private int a;
+    private int b;
+
+    public int multiplicacion(int a, int b){
+        return a*b;
+    }
 }
+
 
