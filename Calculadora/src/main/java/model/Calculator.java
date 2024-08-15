7094621699b4a65fbfba2e94f6e2bc7e0e4448c1
josh.uni99@gmail.com <josh.uni99@gmail.com>
2024-08-14 21:59:48 -0500

    Divi 1

--- Calculadora/src/main/java/model/Calculator.java
+++ Calculadora/src/main/java/model/Calculator.java
@@ -4,5 +4,12 @@ public class Calculator {
 
     public Calculator() {
     }
+
+    private double a;
+    private double b;
+
+    public double division(double a, double b){
+        return a/b;
+    }
 }
 
